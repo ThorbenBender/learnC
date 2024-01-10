a26c2dc4741144f1a59d2163d3f089857cb971a0
Thorben Bender <bender.thorben0@gmail.com>
2024-01-10 21:11:37 +0100

    Binary Tree Example

--- main.c
+++ main.c
@@ -3,38 +3,6 @@
 
 typedef struct node {
   int number;
-  struct node *next;
+  struct node *left;
+  struct node *right;
 } node;
-
-int main(int argc, char *argv[]) {
-  node *list = NULL;
-
-  for (int i = 1; i < argc; i++) {
-    int number = atoi(argv[i]);
-
-    node *n = malloc(sizeof(node));
-
-    if (n == NULL) {
-      return 1;
-    }
-    n->number = number;
-    n->next = NULL;
-
-    n->next = list;
-    list = n;
-  }
-
-  node *point = list;
-
-  while (point != NULL) {
-    printf("Node is %i\n", point->number);
-    point = point->next;
-  }
-
-  point = list;
-  while (point != NULL) {
-    node *next = point->next;
-    free(point);
-    point = next;
-  }
-}
